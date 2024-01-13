0547a78c3ff25e70f25c3245b3c5d46b8db270ec
AnnishaSiddika <annisha2112@gmail.com>
2024-01-13 01:31:48 +0100

    navbar added

---
+++ client/src/components/Shared/Navbar/Avatar.jsx
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import avatar from '../../../assets/images/placeholder.jpg';
+import { AuthContext } from '../../../providers/AuthProvider';
+
+const Avatar = () => {
+    const { user } =useContext(AuthContext)
+    return (
+        <img src={user && user.photoURL ? user.photoURL : avatar} alt="profile" className='rounded-full' width='30' height='30' />
+    );
+};
+
+export default Avatar;
