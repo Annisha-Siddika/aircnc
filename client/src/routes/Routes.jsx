5422a8a51f60044a888e4fa063cd171459a09454
AnnishaSiddika <annisha2112@gmail.com>
2024-01-18 22:03:04 +0100

    added dashboard route

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -5,6 +5,7 @@ import Login from '../pages/Login/Login'
 import SignUp from '../pages/SignUp/SignUp'
 import RoomDetails from '../pages/RoomDetails/RoomDetails'
 import PrivateRoute from './PrivateRoute'
+import DashboardLayout from '../layouts/DashboardLayout'
 
 export const router = createBrowserRouter([
   {
@@ -28,5 +29,15 @@ export const router = createBrowserRouter([
   {
     path: '/signup',
     element: <SignUp/>
+  },
+  {
+    path: '/dashboard',
+    element: <DashboardLayout/>,
+    children: [
+      {
+        path: '/dashboard/add-room',
+        element: <p>this is add room</p>
+      }
+    ]
   }
 ])
